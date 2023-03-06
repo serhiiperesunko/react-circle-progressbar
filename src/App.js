7085f75622b353a750c664db2d4009e04bac2f98
sergiy <serhiiperesunko@gmail.com>
2023-03-06 20:21:44 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import {useState} from "react";
 
 function App() {
   const [value, setValue] = useState(0)
-  console.log(value)
   return <>
     <ProgressBar value={value}/>
-    <input type="text" onChange={(e) => setValue(e.target.value)} onClick={(e) => setValue(e.target.value)}/>
+    <input type="number"
+           min={0}
+           max={100}
+           placeholder="number"
+           style={{
+      position: "relative",
+      top:100,
+      zIndex: 99
+    }} onChange={(e) => setValue(e.target.value)} onClick={(e) => {
+      if(e.target.value !== ""){
+        setValue(e.target.value)
+      } else {
+        setValue(0)
+      }
+
+    }}/>
   </>
 }
 
