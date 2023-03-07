3ca5f6886612cac0b9887c13b97bf157105376e7
sergiy <serhiiperesunko@gmail.com>
2023-03-07 10:51:29 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import ProgressBar from "./components/progress-bar/ProgressBar";
+import {useState} from "react";
 
 function App() {
+  const [value ,setValue] = useState(0)
   return <>
-    <ProgressBar value={100}/>
+    <ProgressBar value={value}/>
+    <div style={{
+      marginTop: 20
+    }}>
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+    </div>
   </>
 }
 
